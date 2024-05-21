0120469f597bef6c415981cce5160c49787aa449
Arthur <arthurmaceddo@gmail.com>
2024-05-20 22:36:43 -0300

    first commit

---
+++ resources/views/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>Pagina não encontrada</h1>
+    <p>A pagina que você está tentando acessar não foi encontrada.</p>
+    <a href= "{{ route ('home') }}"> Voltar a Pagina Inicial</a>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Produtos</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Preço</th>
+                    <th>Estoque</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td> {{ $product-$name}} </td>
+                        <td> {{ $product-$description}} </td>
+                        <td> {{ $product-$price}} </td>
+                        <td> {{ $product-$stock}} </td>
+                        <td>
+                            <a href="{{ route ('products.edit', $product)}}"> Edit </a> 
+                            <form action=" {{route ('products.destroy', $product)}}" method="POST" style="display: inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endsection
